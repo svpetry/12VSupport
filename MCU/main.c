466730e71f0e1f6c788393de6b3436f8c196aa1b
SP <svpetry@gmail.com>
2024-11-30 10:32:33 +0100

    Added main file, added config bits.

---
+++ MCU/main.c
@@ -0,0 +1,41 @@
+/*
+ * File:   main.c
+ * Author: svpet
+ *
+ * Created on 30. November 2024, 10:10
+ * 
+ * pin layout:
+ * RA0: 12_SENSE (AN0)
+ * RA1: TEMP_SENSE (AN1)
+ * RA2: BAT_SENSE (AN2)
+ * RA3: CURR_SENSE (AN3)
+ * RA4: NC
+ * RA5: NC
+ * RA6: OSC2
+ * RA7: OSC1
+ * RC0: REL_CHARGE (output)
+ * RC1: REL_BOOST_IN (output)
+ * RC2: REL_BUCK_OUT (output)
+ * RC3: REL_HEATER (output)
+ * RC4: NC
+ * RC5: NC
+ * RC6: LED red
+ * RC7: LED green
+ * RE3: VPP
+ * RB0: SOC LED 20%
+ * RB1: SOC LED 40%
+ * RB2: SOC LED 60%
+ * RB3: SOC LED 80%
+ * RB4: SOC LED 100%
+ * RB5: NC
+ * RB6: PGC
+ * RB7: PGD
+ */
+
+
+#include <xc.h>
+
+void main(void) {
+    
+    return;
+}
