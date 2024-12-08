23cdc7c5d62f3cb2786ec68f2c6118e790e2a2b0
SP <svpetry@gmail.com>
2024-12-08 11:50:09 +0100

    MCU: Save calculated full capacity to EEPROM.

--- MCU/main.c
+++ MCU/main.c
@@ -49,6 +49,8 @@
 // capacity values
 long rem_cap; // remaining capacity in uAh
 long full_cap; // full capacity in mAh
+int soc = 0; // current calculated SOC
+
 int soc_level_count = 16;
 long soc_voltage_levels[16] = {
     29200,
@@ -120,7 +122,6 @@ void Initialize() {
     // Enable global and peripheral interrupts
     INTCONbits.PEIE = 1;    // Enable Peripheral Interrupts
     INTCONbits.GIE = 1;     // Enable Global Interrupts
-
 }
 
 long GuessRemainingCap() {
@@ -152,20 +153,35 @@ void CountRemainingCapacity() {
         rem_cap = 0;
 }
 
+void CalcSoc() {
+    soc = (int)(rem_cap / (10 * full_cap));
+}
+
 void SetSocLeds() {
     unsigned char portb = 0;
-    int socPercent = (int)(rem_cap / (10 * full_cap));
-    if (socPercent > 0)
+    if (soc > 0)
         portb |= 0x01;
-    if (socPercent > 20)
+    if (soc > 20)
         portb |= 0x02;
-    if (socPercent > 40)
+    if (soc > 40)
         portb |= 0x04;
-    if (socPercent > 60)
+    if (soc > 60)
         portb |= 0x08;
-    if (socPercent > 80)
+    if (soc > 80)
         portb |= 0x10;
     LATB = portb;
+}
+
+long InitFullCap() {
+    if (EEPROM_Read_32Bit(EEPROM_ADDR_VERSION) == EEPROM_VERSION) 
+        return EEPROM_Read_32Bit(EEPROM_ADDR_FULL_CAP);
+    return INITIAL_FULL_CAP;
+}
+
+void SetFullCap(long value) {
+    full_cap = value;
+    EEPROM_Write_32Bit(EEPROM_ADDR_VERSION, EEPROM_VERSION);
+    EEPROM_Write_32Bit(EEPROM_ADDR_FULL_CAP, value);
 }
 
 void MainLoop() {
@@ -175,6 +191,7 @@ void MainLoop() {
     
     if (state != STATE_INITIAL) {
         CountRemainingCapacity();
+        CalcSoc();
         SetSocLeds();
     }
     
@@ -183,7 +200,7 @@ void MainLoop() {
             LATC = 0;
             LATB = 0;
             if (batt_voltage > BATT_MIN_VOLTAGE) {
-                full_cap = INITIAL_FULL_CAP;
+                full_cap = InitFullCap();
                 rem_cap = GuessRemainingCap();
                 state = STATE_READY;
             } else {
@@ -198,7 +215,7 @@ void MainLoop() {
             if (system_voltage < VOLTAGE_START_SUPPLY) {
                 LATCbits.LATC6 = 0; // red LED
                 state = STATE_SUPPLYING;
-            } else if (system_voltage > VOLTAGE_START_CHARGING) {
+            } else if (system_voltage > VOLTAGE_CHARGE_IF_NEEDED) {
                 LATCbits.LATC6 = 0; // red LED
                 state = STATE_CHARGING;
             } else if (batt_voltage <= BAT_VOLTAGE_EMPTY)
@@ -211,7 +228,7 @@ void MainLoop() {
             LATCbits.LATC7 = 1; // green LED
             LATCbits.LATC2 = 1; // buck output relay
 
-            if (batt_voltage > VOLTAGE_START_CHARGING) {
+            if (batt_voltage > VOLTAGE_CHARGE_IF_NEEDED) {
                 stop_supply = 1;
                 state = STATE_CHARGING;
             } else if (batt_current < BAT_CURRENT_THRESHOLD && batt_voltage < BAT_VOLTAGE_EMPTY) {
@@ -229,12 +246,12 @@ void MainLoop() {
 
         case STATE_EMPTY: {
             if (!cap_reset_empty) {
-                full_cap -= rem_cap / 1000;
+                SetFullCap(full_cap - rem_cap / 1000);
                 rem_cap = 0;
                 cap_reset_empty = 1;
             }
             LATCbits.LATC7 = sec; // green LED
-            if (batt_voltage > VOLTAGE_START_CHARGING) {
+            if (batt_voltage > VOLTAGE_CHARGE_IF_NEEDED) {
                 LATCbits.LATC7 = 0; // green LED
                 state = STATE_CHARGING;
             }
@@ -247,7 +264,8 @@ void MainLoop() {
             else 
                 LATCbits.LATC3 = 0; // heater relay
             
-            if (batt_temp >= CHARGING_MIN_TEMP) {
+            if (batt_temp >= CHARGING_MIN_TEMP && 
+                    (system_voltage > VOLTAGE_FORCE_CHARGING || soc < SOC_CHARGE_IF_NEEDED)) {
                 if (!PORTCbits.RC0) {
                     LATCbits.LATC6 = 1; // red LED
                     LATCbits.LATC1 = 1; // boost converter input relay
@@ -281,7 +299,7 @@ void MainLoop() {
         case STATE_FULL: {
             if (cap_reset_empty) {
                 cap_reset_empty = 0;
-                full_cap = rem_cap / 1000;
+                SetFullCap(rem_cap / 1000);
             } else
                 rem_cap = full_cap * 1000;
 
